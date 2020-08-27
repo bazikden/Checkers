4e3943a11f34aa00511824761ffefb8e75e44cf6
Denis Bazilevsky <denis.bazilevsky@qbex.io>
2020-08-27 14:17:09 +0300

    make deploy

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ var express_1 = __importDefault(require("express"));
 var database_1 = __importDefault(require("./utils/database"));
 var rooms_1 = __importDefault(require("./routes/rooms"));
 var socket_1 = __importDefault(require("./utils/socket"));
+var path_1 = __importDefault(require("path"));
 var app = express_1.default();
 var server = socket_1.default(app);
 // Middleware
@@ -16,4 +17,10 @@ app.use(express_1.default.json());
 database_1.default.on('connect', function () { return console.log('Database connected'); });
 database_1.default.on('error', function () { return console.log('Failed to connect db...'); });
 app.use('/api/rooms', rooms_1.default);
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+    app.use(express_1.default.static('../client/build'));
+    app.get('*', function (req, res) {
+        res.sendFile(path_1.default.join(__dirname + '../client/build/index.html'));
+    });
+}
 server.listen(process.env.PORT, function () { return console.log("Server is started on port " + process.env.PORT); });
--- server/index.ts
+++ server/index.ts
@@ -4,6 +4,7 @@ import express from 'express'
 import db from './utils/database'
 import rooms from './routes/rooms'
 import createServer from './utils/socket'
+import path from 'path'
 const app: express.Application = express()
 
 const server = createServer(app)
@@ -18,5 +19,11 @@ db.on('error', () => console.log('Failed to connect db...'))
 
 app.use('/api/rooms', rooms)
 
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+    app.use(express.static('../client/build'));
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname + '../client/build/index.html'));
+    });
+  }
 
 server.listen(process.env.PORT, () => console.log(`Server is started on port ${process.env.PORT}`))
